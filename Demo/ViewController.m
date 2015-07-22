79c2900e10cdf7970020d057855e5771d6d48981
KieuPTN <ptnkieu.sj13@gmail.com>
2015-07-22 16:58:56 +0700

    no message

--- Demo/ViewController.m
+++ Demo/ViewController.m
@@ -10,6 +10,9 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) CBCentralManager *centralManager;
+@property (nonatomic, strong) CBPeripheral *periplheral;
+
 @end
 
 @implementation ViewController
@@ -17,11 +20,19 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    _centralManager = [[CBCentralManager alloc]initWithDelegate:self queue:nil];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)centralManagerDidUpdateState:(CBCentralManager *)central {
+    if (central.state == CBCentralManagerStatePoweredOn) {
+        [_centralManager scanForPeripheralsWithServices:nil options:@{ CBCentralManagerScanOptionAllowDuplicatesKey : @YES }];
+        NSLog(@"Scan");
+    }
 }
 
 @end
